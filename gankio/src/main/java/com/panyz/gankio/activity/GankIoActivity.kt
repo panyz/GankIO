5200bf8e1bdf5f167b43f2343b39758152888103
panyz <869997807@qq.com>
2021-02-01 13:29:04 +0800

    创建GankIO组件模块

---
+++ gankio/src/main/java/com/panyz/gankio/activity/GankIoActivity.kt
@@ -0,0 +1,12 @@
+package com.panyz.gankio.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.panyz.gankio.R
+
+class GankIoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_gank_io)
+    }
+}
